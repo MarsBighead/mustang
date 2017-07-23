c19c99a30e54f4df48cc22328667d84ec5171bc0
MarsBighead <duanmhy@126.com>
2017-07-23 18:14:49 +0800

    Improve output reverse with awk, and output special lines

---
+++ Shell/exclude_spec_lin.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+#Output from line two
+awk 'NR>1'  data.txt 
+
+
--- Shell/reserver_out.sh
+++ Shell/reserver_out.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
+
+#tac reverse output
 ls -tl | awk '{if ( $9!="" ) {print $9}}' |tac
 
+#Remove blank rows with awk NF
+ls -lt | awk  '{print $9}' | awk NF | tac
 
+#Output with ls parameter -r
 ls  -tr | awk '{print $1 }'
 
