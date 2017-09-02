6be1340eb25f2537c1fb8e975532f1041a7a9e7b
MarsBighead <duanmhy@126.com>
2017-09-02 22:25:34 +0800

    Toger#Perl function ref() samples

--- module_test.pl
+++ module_test.pl
@@ -7,3 +7,13 @@ use Mars::Mustang;
 my $status_code=mg_toger();
 print "$status_code\n";
 
+my $a = [1,2,3,4,5];
+print "type \$a ".ref($a)."\n";
+my $h = {
+    "paul"=>"duan",
+    "num" => $a,
+};
+
+print Dumper($h);
+print "type \$h ".ref($h)."\n";
+print "type \$h->{num} ".ref($h->{"num"})."\n";
