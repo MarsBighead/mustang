15aa5c0ee300c9f5930873de89149c0d03afc4a3
MarsBighead <duanmhy@126.com>
2017-09-11 18:29:33 +0800

    Shell sample case-esac script

---
+++ Shell/case_test.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+case $USER in
+duanp | hbu )
+	echo "weclome, $USER"
+	echo "Please enjoy your vissit";;
+testing)
+	echo "Specail testing account";;
+jessica)
+	echo "Do not forget to log off when you're done";;
+*)
+	echo "Sorry, you are not allowed here";;
+esac
