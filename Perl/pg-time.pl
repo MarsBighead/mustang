cf54ad0f602337280842b02ddaa41d0f941cf431
MarsBighead <duanmhy@126.com>
2017-09-21 22:36:40 +0800

    Toger#Pg time format compute

--- Perl/pg-time.pl
+++ Perl/pg-time.pl
@@ -3,8 +3,28 @@ use warnings;
 use strict;
 use DateTime::Format::Pg;
 use Data::Dumper;
+use DateTime::Format::Strptime;
+
 my $dt = DateTime::Format::Pg->parse_datetime( '2017-07-16 23:12:01' );
 
-print Dumper($dt);
+#print Dumper($dt);
+#$dt->add( days => 1 )->subtract( seconds => 20 );
+$dt->add( hours => 12 )->add( seconds => 20 );
+#print Dumper($dt->formatter());
 #       2003-01-16 23:12:0
-#       DateTime::Format::Pg->format_datetime($dt);
+
+
+=pod
+my $strp = DateTime::Format::Strptime->new();
+=cut
+my $strp = DateTime::Format::Strptime->new(
+        pattern   => '%T',
+        locale    => 'en_AU',
+        time_zone => 'Australia/Melbourne',
+        );
+
+
+print $strp->format_datetime($dt)."\n";
+
+my $ft= DateTime::Format::Pg->format_datetime($dt);
+print "$ft\n";
