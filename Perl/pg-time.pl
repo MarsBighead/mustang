d45159a50020f3d72b7e51759cd5b10a0d6f9cc1
MarsBighead <duanmhy@126.com>
2017-09-23 23:18:36 +0800

    Add compute duration time

--- Perl/pg-time.pl
+++ Perl/pg-time.pl
@@ -4,9 +4,11 @@ use strict;
 use DateTime::Format::Pg;
 use Data::Dumper;
 use DateTime::Format::Strptime;
+use DateTime::Duration;
 
 my $dt = DateTime::Format::Pg->parse_datetime( '2017-07-16 23:12:01' );
 
+my $dt1=DateTime::Format::Pg->parse_datetime( '2017-07-16 23:12:01' );
 #print Dumper($dt);
 #$dt->add( days => 1 )->subtract( seconds => 20 );
 $dt->add( hours => 12 )->add( seconds => 20 );
@@ -28,3 +30,9 @@ print $strp->format_datetime($dt)."\n";
 
 my $ft= DateTime::Format::Pg->format_datetime($dt);
 print "$ft\n";
+my $dur=$dt-$dt1;
+print Dumper($dur);
+print $dt->datetime."\n";
+print $dt1->datetime."\n";
+my %hash=$dur->deltas();
+print Dumper(\%hash);
