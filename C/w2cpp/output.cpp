5207f50db1b5c19be829fcaf9b879f973fd91576
MarsBighead <duanmhy@126.com>
2017-11-02 23:13:56 +0800

    More C++ types

--- C/w2cpp/output.cpp
+++ C/w2cpp/output.cpp
@@ -2,12 +2,20 @@
 
 int sum();
 
+int kickoff();
+
 int main(){
-	std::cout<< "/*";
-	std::cout<< "*/";
-	//std::cout<< /* "*/" */;
-	std::cout<< /* "*/" /* "/*" */;
-	return sum();
+    int num=10, *i;
+    //i=&num;
+    std::cout <<"uninitilze int i is "<<*i<<" address of num is "<<&num<<'\n';
+    std::cout <<"\\115 is "<<'\115'<<'\n';
+    std::cout <<"\\x4d is "<<'\x4d'<<'\n';
+    std::cout <<"\\7 is "<<'\7'<<'\n';
+    std::cout <<"\\7 is "<<'\7'<<'\n';
+    std::cout <<"L'a' is "<<L'a'<<'\n';
+    std::cout <<"u8\"hi!\" is "<<u8"hi!"<<'\n';
+    std::cout <<"1E-3F is "<<1E+3F<<'\n';
+    std::cout <<"\a1E-3F is "<<1E+3F<<'\n';
 }
 
 int sum(){
@@ -19,4 +27,12 @@ int sum(){
     std::cout <<"Sum of 1 to 10 inclusive is"
 	      <<sum<<std::endl;
     return 0;
+}
+
+int kickoff(){
+	std::cout<< "/*";
+	std::cout<< "*/";
+	//std::cout<< /* "*/" */;
+	std::cout<< /* "*/" /* "/*" */;
+	return sum();
 }
