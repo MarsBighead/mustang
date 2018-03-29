0f72aaab3b60c67769075e5499e49d19efa69873
MarsBighead <duanmhy@126.com>
2018-03-29 09:33:53 +0800

    Add Java sample code

---
+++ Java/TestDog.java
@@ -0,0 +1,25 @@
+class Animal{
+   public void move(){
+      System.out.println("动物可以移动");
+   }
+}
+ 
+class Dog extends Animal{
+   public void move(){
+      System.out.println("狗可以跑和走");
+   }
+   public void bark(){
+      System.out.println("狗可以吠叫");
+   }
+}
+ 
+public class TestDog{
+   public static void main(String args[]){
+      Animal a = new Animal(); // Animal 对象
+      Animal b = new Dog(); // Dog 对象
+ 
+      a.move();// 执行 Animal 类的方法
+      b.move();//执行 Dog 类的方法
+      //b.bark();
+   }
+}
