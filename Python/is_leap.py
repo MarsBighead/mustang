89bb0de1f2cb739a8120c1a14aba44f34b2a230c
MarsBighead <duanmhy@126.com>
2018-04-24 18:21:29 +0800

    Toger#Check if a year is leap

---
+++ Python/is_leap.py
@@ -0,0 +1,15 @@
+#/usr/bin/python
+
+def is_leap(year):
+    leap = False
+    
+    # Write your logic here
+    if year%100==0 and year%400==0:
+        leap = True
+    elif year%4==0 and year%100!=0:
+        leap = True
+    
+    return leap
+
+year = int(raw_input())
+print is_leap(year)
