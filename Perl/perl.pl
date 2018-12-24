f2e5233066900b9e39af76132e78d1ba9116a1a7
MarsBighead <duanmhy@126.com>
2018-12-24 16:35:09 +0800

    Add perl function case

--- Perl/perl.pl
+++ Perl/perl.pl
@@ -4,8 +4,14 @@ use strict;
 use warnings;
 
 use Data::Dumper;
+my $arr=[(1..10)];
+print Dumper($arr);
+print  ref($arr)."\n";
 my $string="abcd,acd,tgc,sss,ug";
 my @arr=split /\,/,$string,-1;
+print  ref(\@arr)."| ".length($arr[0])." plain\n";
+my $string="abcd,acd,tgc,sss,ug";
+print length(@arr)." length\n";
 print "Arr\t@arr\n";
 
 my @ref=\(1,2,3);
@@ -21,6 +27,7 @@ my $hash={
    "array"=>[1,2,3,4]
 };
 
+print length(%$hash)." length of hash\n";
 print Dumper($hash);
 print "A".."Z";
 
