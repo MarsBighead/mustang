395c66730ee3f77c5a44475fc0998eb59896be0e
MarsBighead <duanmhy@126.com>
2018-12-29 17:03:43 +0800

    Add Perl typeglobs example

--- Perl/test-my.pl
+++ Perl/test-my.pl
@@ -19,3 +19,16 @@ sub calculate_test{
     my ($template , $num)=@_;
     print "\$num is $num\n";
 }
+#https://www.cs.ait.ac.th/~on/O/oreilly/perl/advprog/ch03_02.htm
+# Unable to apply with variable which has been declared by my
+#Typeglobs, we mentioned earlier, can be localized (with local only) and assigned to one another. Assigning a typeglob has the effect of aliasing one identifier name to another. 
+$array = 1;
+@array = (1..10);
+$spud   = "Wow!";
+@spud   = ("idaho", "russet");
+*potato = *spud;   # Alias potato to spud using typeglob assignment
+print "$potato\n"; # prints "Wow!"
+print @potato, "\n"; # prints "idaho russet"
+
+*protato = *array;
+print "@protato\n";
