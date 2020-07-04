4f717dba20c2114dfb96f98c591174a1462f133f
MarsBighead <duanmhy@126.com>
2020-07-04 16:00:10 +0800

    Add docker and Python3 sample

---
+++ docker/Jenkins/init.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+docker network create jenkins
+docker volume create jenkins-docker-certs
+docker volume create jenkins-data
+docker container run \
+  --name jenkins-docker \
+  --detach \
+  --privileged \
+  --network jenkins \
+  --network-alias docker \
+  --env DOCKER_TLS_CERTDIR=/certs \
+  --volume jenkins-docker-certs:/certs/client \
+  --volume jenkins-data:/var/jenkins_home \
+  --publish 2376:2376 \
+  docker:dind
+
+#delete  --rm \
+docker container run \
+  --name jenkins-blueocean \
+  --detach \
+  --network jenkins \
+  --env DOCKER_HOST=tcp://docker:2376 \
+  --env DOCKER_CERT_PATH=/certs/client \
+  --env DOCKER_TLS_VERIFY=1 \
+  --publish 8080:8080 \
+  --publish 50000:50000 \
+  --volume jenkins-data:/var/jenkins_home \
+  --volume jenkins-docker-certs:/certs/client:ro \
+  jenkinsci/blueocean
