8e28977a4765c428be861ed133fb9ecf3bbbf902
MarsBighead <duanmhy@126.com>
2021-09-09 22:03:12 +0800

    Improve Archey installation process and add awk sample

---
+++ Shell/test.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+awk 'BEGIN{printf "序号\t名字\t课程\t分数\n"} {print}' marks.txt
+
--- ansible/roles/archery/templates/init-database.sh
+++ ansible/roles/archery/templates/init-database.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-CREATE DATABASE `archery1` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+CREATE USER IF NOT EXISTS archery@'%' IDENTIFIED BY 'Archery!23';
+CREATE DATABASE `archery` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+GRANT ALL ON archery.* to  archery@'%';
+FLUSH PRIVILEGES;
