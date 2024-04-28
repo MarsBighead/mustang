83ab4e3a91db34241aedae559fc44172f94a2a8b
MarsBighead <duanmhy@126.com>
2024-04-28 17:14:20 +0800

    Add case to remove duplicates from sorted list

--- Go/pkg/linked/reverse.go
+++ Go/pkg/linked/reverse.go
@@ -1,6 +1,8 @@
 package linked
 
-import "fmt"
+import (
+	"fmt"
+)
 
 type ListNode struct {
 	Val  int
@@ -10,6 +12,24 @@ type ListNode struct {
 func reverseList(head *ListNode) *ListNode {
 	var cur *ListNode
 	return reverse(cur, head)
+}
+
+func reverseList2(head *ListNode) *ListNode {
+	var prev *ListNode
+	current := head
+	for current != nil {
+		// 真正列表的next内容
+		next := current.Next
+
+		// 反转next指向prev
+		current.Next = prev
+		// prev反转增加current节点
+		prev = current
+
+		// current指向下一个节点
+		current = next
+	}
+	return prev
 }
 
 func reverse(prev, cur *ListNode) *ListNode {
@@ -38,6 +58,10 @@ func ArrayToList(nums []int) *ListNode {
 }
 
 func PrintList(head *ListNode) {
+	if head == nil {
+		fmt.Println()
+		return
+	}
 	if head != nil {
 		fmt.Print(head.Val, " ")
 	}
@@ -46,4 +70,57 @@ func PrintList(head *ListNode) {
 		fmt.Print(head.Val, " ")
 	}
 	fmt.Println()
+	return
+}
+
+// https://leetcode.cn/problems/remove-duplicates-from-sorted-list/description/
+func deleteDuplicates(head *ListNode) *ListNode {
+	node := head
+	for node.Next != nil {
+		fmt.Println(node.Val)
+		if node.Val == node.Next.Val {
+			node.Next = node.Next.Next
+		} else {
+			node = node.Next
+		}
+
+		if node.Next == nil {
+			break
+		}
+
+	}
+	return head
+}
+
+// https://leetcode.cn/problems/remove-duplicates-from-sorted-list-ii/
+func deleteAllDuplicates(head *ListNode) *ListNode {
+	if head == nil {
+		return nil
+	}
+	// 创建一个虚拟头结点
+	dummy := &ListNode{Next: head}
+	next := head
+	current := dummy
+	for next != nil {
+		flag := false
+		// 当前节点值
+		val := next.Val
+		// 下一个节点
+
+		// 如果下一个节点存在并且值与当前节点值相同，则一直移动next直到值不同
+		for next.Next != nil && val == next.Next.Val {
+			next = next.Next
+			flag = true
+		}
+		// 如果next为空，表示所有重复节点都被跳过了，直接链接
+		if flag {
+			current.Next = next.Next
+		} else {
+			current.Next = next
+			current = next
+		}
+		// 移动到下一个节点
+		next = next.Next
+	}
+	return dummy.Next
 }
