e1b0af63923d05ecc6c4784b32923021dcf27aed
MarsBighead <duanmhy@126.com>
2024-04-29 11:24:06 +0800

    Add palindrome linked list cases

--- Go/pkg/linked/reverse.go
+++ Go/pkg/linked/reverse.go
@@ -172,3 +172,17 @@ func deleteAllDuplicates(head *ListNode) *ListNode {
 	}
 	return dummy.Next
 }
+
+// https://leetcode.cn/problems/remove-linked-list-elements/
+func removeElements(head *ListNode, val int) *ListNode {
+	dummy := &ListNode{Next: head}
+	prev := dummy
+	for prev.Next != nil {
+		if prev.Next.Val == val {
+			prev.Next = prev.Next.Next
+		} else {
+			prev = prev.Next
+		}
+	}
+	return dummy.Next
+}
