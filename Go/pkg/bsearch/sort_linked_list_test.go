29d0347796c29dfffed16ce9c8457eaf89a60dcb
MarsBighead <duanmhy@126.com>
2024-04-30 17:03:47 +0800

    Add merge k sorted lists case

--- Go/pkg/bsearch/sort_linked_list_test.go
+++ Go/pkg/bsearch/sort_linked_list_test.go
@@ -24,3 +24,33 @@ func TestHasCycle(t *testing.T) {
 	ok := hasCycle(head)
 	fmt.Println(ok)
 }
+
+func TestSortList(t *testing.T) {
+	nums := []int{4, 2, 1, 3}
+	head := array2List(nums)
+	PrintList(head)
+	sorted := sortList(head)
+	PrintList(sorted)
+}
+
+func TestMergeKLists(t *testing.T) {
+	//nums := [][]int{{1, 4, 5}, {1, 3, 4}, {2, 6}}
+	nums := [][]int{{}, {1, 4, 5}, {}, {2, 6}}
+	var lists []*ListNode
+	for _, row := range nums {
+		head := array2List(row)
+		lists = append(lists, head)
+
+	}
+	sorted := mergeKListsV2(lists)
+	PrintList(sorted)
+}
+
+func TestMergeSort(t *testing.T) {
+	nums := []int{4, 2, 1, 3, 5}
+	head := array2List(nums)
+	PrintList(head)
+	sorted := mergeSort(head)
+	fmt.Println("result:")
+	PrintList(sorted)
+}
