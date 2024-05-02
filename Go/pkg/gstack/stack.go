1ffd90c05b5126d8c6173e37de8395caa42ee78a
MarsBighead <duanmhy@126.com>
2024-05-02 21:17:32 +0800

    Add mini stack case

--- Go/pkg/gstack/stack.go
+++ Go/pkg/gstack/stack.go
@@ -1,5 +1,62 @@
 package gstack
 
+type MinStack struct {
+	Stack []int
+	// Diff=Min-PrevMin
+	Min  []int
+	size int
+}
+
+// https://leetcode.cn/problems/min-stack/description/
+func Constructor() MinStack {
+	return MinStack{
+		Stack: []int{},
+		Min:   []int{},
+	}
+
+}
+
+func (this *MinStack) Push(val int) {
+	if len(this.Stack) == 0 {
+		this.Stack = append(this.Stack, val)
+		this.Min = append(this.Min, val)
+	} else {
+		min := this.Min[len(this.Min)-1]
+		if val < min {
+			min = val
+		}
+		this.Stack = append(this.Stack, val)
+		this.Min = append(this.Min, min)
+	}
+	this.size += 1
+}
+
+func (this *MinStack) Pop() {
+	if len(this.Stack) == 0 {
+		return
+	} else {
+		this.Stack = this.Stack[:this.size-1]
+		this.Min = this.Min[:this.size-1]
+		this.size -= 1
+	}
+}
+
+func (this *MinStack) Top() int {
+	if len(this.Stack) == 0 {
+		return 0
+	} else {
+		return this.Stack[this.size-1]
+	}
+
+}
+
+func (this *MinStack) GetMin() int {
+	if len(this.Stack) == 0 {
+		return 0
+	}
+	return this.Min[this.size-1]
+}
+
 // https://leetcode.cn/problems/remove-all-adjacent-duplicates-in-string/description/
 // 删除字符串中相邻重复的字符
 func removeDuplicates(s string) string {
