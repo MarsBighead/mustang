58a30a1993b64df1739aa99655b694f8078a83ae
MarsBighead <duanmhy@126.com>
2024-05-05 17:07:26 +0800

    Add basic calculator

--- Go/pkg/gstack/stack_test.go
+++ Go/pkg/gstack/stack_test.go
@@ -20,3 +20,15 @@ func TestIsValid(t *testing.T) {
 	ok := isValid(s)
 	fmt.Println(ok)
 }
+
+func TestCalculte(t *testing.T) {
+	s := " 0-2147483647"
+	r := calculate(s)
+	fmt.Println(s, r)
+}
+
+func TestCalcultev2(t *testing.T) {
+	s := " 0-2147483647"
+	r := calculatev2(s)
+	fmt.Println(s, r)
+}
