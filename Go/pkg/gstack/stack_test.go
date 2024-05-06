688b2a6fb4b5193fd03c205280064b12e44949de
MarsBighead <duanmhy@126.com>
2024-05-06 19:11:12 +0800

    Add daily temperature case

--- Go/pkg/gstack/stack_test.go
+++ Go/pkg/gstack/stack_test.go
@@ -32,3 +32,9 @@ func TestCalcultev2(t *testing.T) {
 	r := calculatev2(s)
 	fmt.Println(s, r)
 }
+
+func TestDailyTemperatures(t *testing.T) {
+	temperatures := []int{73, 74, 75, 71, 69, 72, 76, 73}
+	ans := dailyTemperatures(temperatures)
+	fmt.Println(ans)
+}
