5ee5f65219f45d1449720f2db0a5ef8b948a08db
MarsBighead <duanmhy@126.com>
2024-09-26 13:58:59 +0800

    Find first missing positive case#41

--- Go/pkg/garray/double_digit.go
+++ Go/pkg/garray/double_digit.go
@@ -131,3 +131,53 @@ func CombinationSum2(candidates []int, target int) (ans [][]int) {
 	dfs(target, 0)
 	return
 }
+
+// https://leetcode.cn/problems/first-missing-positive/description/
+// 41. 缺失的第一个正数
+func FirstMissingPositive(nums []int) int {
+	n := len(nums)
+	fmt.Println(nums)
+	for i := 0; i < n; i++ {
+		for nums[i] > 0 && nums[i] <= n && nums[nums[i]-1] != nums[i] {
+			nums[nums[i]-1], nums[i] = nums[i], nums[nums[i]-1]
+		}
+	}
+	fmt.Println(nums)
+	for i := 0; i < n; i++ {
+		if nums[i] != i+1 {
+			return i + 1
+		}
+	}
+	return n + 1
+}
+
+func FirstMissingPositiveV1(nums []int) int {
+	n := len(nums)
+	fmt.Println(nums)
+	for i := 0; i < n; i++ {
+		if nums[i] <= 0 {
+			nums[i] = n + 1
+			//continue
+		}
+	}
+	for i := 0; i < n; i++ {
+		num := abs(nums[i])
+		if num <= n {
+			nums[num-1] = -abs(nums[num-1])
+		}
+	}
+	fmt.Println("--", nums)
+	for i := 0; i < n; i++ {
+		if nums[i] > 0 {
+			return i + 1
+		}
+	}
+	return n + 1
+}
+
+func abs(x int) int {
+	if x < 0 {
+		return -x
+	}
+	return x
+}
