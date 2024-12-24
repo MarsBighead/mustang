dbfdf18b4cf302bdaf38791295d21c22503c7780
MarsBighead <duanmhy@126.com>
2024-12-24 10:51:12 +0800

    Add next permutation

---
+++ Go/pkg/garray/permutation.go
@@ -0,0 +1,24 @@
+package garray
+
+func NextPermutation(nums []int) {
+
+	n := len(nums)
+	i := n - 2
+	for i >= 0 && nums[i] >= nums[i+1] {
+		i--
+	}
+	if i >= 0 {
+		j := n - 1
+		for j >= 0 && nums[i] >= nums[j] {
+			j--
+		}
+		nums[i], nums[j] = nums[j], nums[i]
+	}
+	reverse(nums[i+1:])
+}
+
+func reverse(a []int) {
+	for i, n := 0, len(a); i < n/2; i++ {
+		a[i], a[n-1-i] = a[n-1-i], a[i]
+	}
+}
