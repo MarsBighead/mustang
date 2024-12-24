2a20ef721d9ec47c787049df88de27d6f3db48c9
MarsBighead <duanmhy@126.com>
2024-12-24 14:33:00 +0800

    Add Valid IP address case

--- Go/pkg/garray/permutation.go
+++ Go/pkg/garray/permutation.go
@@ -1,5 +1,7 @@
 package garray
 
+import "regexp"
+
 func NextPermutation(nums []int) {
 
 	n := len(nums)
@@ -21,4 +23,19 @@ func reverse(a []int) {
 	for i, n := 0, len(a); i < n/2; i++ {
 		a[i], a[n-1-i] = a[n-1-i], a[i]
 	}
+}
+
+// 468.验证IP地址
+// https://leetcode.cn/problems/validate-ip-address/description/
+func ValidIPAddress(queryIP string) string {
+	var (
+		ipv4 = regexp.MustCompile("^(?:(?:0\\.){3}0)|^((?:25[0-5]|2[0-4]\\d|((1\\d{2})|[1-9]?\\d|))((?:\\.)(?:25[0-5]|2[0-4]\\d|((1\\d{2})|([1-9]?\\d)))){3})$")
+		ipv6 = regexp.MustCompile("^([0-9a-fA-F]{1,4})(\\:[0-9a-fA-F]{1,4}){7}$")
+	)
+	if ipv4.MatchString(queryIP) {
+		return "IPv4"
+	} else if ipv6.MatchString(queryIP) {
+		return "IPv6"
+	}
+	return "Neither"
 }
