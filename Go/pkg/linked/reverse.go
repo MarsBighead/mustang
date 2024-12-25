f881078a1fd6a72fa0accace5c06812ee3d0bedd
MarsBighead <duanmhy@126.com>
2024-12-25 20:45:03 +0800

    Reverse book list

--- Go/pkg/linked/reverse.go
+++ Go/pkg/linked/reverse.go
@@ -185,3 +185,55 @@ func removeElements(head *ListNode, val int) *ListNode {
 	}
 	return dummy.Next
 }
+
+// LCR 123. 图书整理 I
+// https://leetcode.cn/problems/cong-wei-dao-tou-da-yin-lian-biao-lcof/description/
+func reverseBookList(head *ListNode) []int {
+	if head == nil {
+		return []int{}
+	}
+
+	current := head
+	cnt := 0
+	for current.Next != nil {
+		cnt++
+		current = current.Next
+	}
+	ans := make([]int, cnt+1)
+	current = head
+	for current.Next != nil {
+		ans[cnt] = current.Val
+		current = current.Next
+		cnt--
+	}
+	ans[cnt] = current.Val
+
+	return ans
+}
+func reverseBookListv1(head *ListNode) []int {
+
+	if head == nil {
+		return []int{}
+	}
+	var (
+		prev *ListNode
+		cnt  int
+	)
+
+	for head != nil {
+		tmp := head.Next
+		head.Next = prev
+		prev = head
+		head = tmp
+		cnt++
+	}
+
+	ans := make([]int, cnt)
+	cnt = 0
+	for head != nil {
+		ans[cnt] = head.Val
+		head = head.Next
+		cnt++
+	}
+	return ans
+}
