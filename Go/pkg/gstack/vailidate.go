238625f5abd69509c31c3dccf840a056ff8de718
MarsBighead <duanmhy@126.com>
2024-12-26 10:12:22 +0800

    Add montone incresing and descreasing stack template

--- Go/pkg/gstack/vailidate.go
+++ Go/pkg/gstack/vailidate.go
@@ -41,3 +41,30 @@ func validateStackSequences(pushed []int, popped []int) bool {
 
 	return ok
 }
+
+// 3. 单调栈模板
+// 3.1 单调递增栈模板
+func monotoneIncreasingStack(nums []int) []int {
+	stack := []int{}
+	for _, num := range nums {
+		n := len(stack)
+		for n > 0 && num >= stack[n-1] {
+			stack = stack[:n-1]
+		}
+		stack = append(stack, num)
+	}
+	return stack
+}
+
+// 3.2 单调递减栈模板
+func monotoneDecreasingStack(nums []int) []int {
+	stack := []int{}
+	for _, num := range nums {
+		n := len(stack)
+		for n > 0 && num <= stack[n-1] {
+			stack = stack[:n-1]
+		}
+		stack = append(stack, num)
+	}
+	return stack
+}
