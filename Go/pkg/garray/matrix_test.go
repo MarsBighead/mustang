b0bffc245afd70291a532c9693dd1b05cb50a6ae
MarsBighead <duanmhy@126.com>
2024-12-27 10:48:12 +0800

    K closest points toorigin

--- Go/pkg/garray/matrix_test.go
+++ Go/pkg/garray/matrix_test.go
@@ -10,3 +10,13 @@ func TestSpiralOrder(t *testing.T) {
 	nums := spiralOrder(mx)
 	fmt.Println(nums)
 }
+
+func TestIsPossibleDivide(t *testing.T) {
+	ks := []int{4}
+	nss := [][]int{{1, 2, 3, 3, 4, 4, 5, 6}}
+	for i, nums := range nss {
+		k := ks[i]
+		ok := isPossibleDivide(nums, k)
+		fmt.Println("ok=", ok)
+	}
+}
---
+++ Go/pkg/gstack/space.go
@@ -0,0 +1,51 @@
+package gstack
+
+import (
+	"fmt"
+	"sort"
+)
+
+// 973. 最接近原点的 K 个点
+// https://leetcode.cn/problems/k-closest-points-to-origin/description/
+func kClosest(points [][]int, k int) [][]int {
+	if len(points) == 1 && k == 1 {
+		return points
+	}
+	distances := []int{}
+	hash := make(map[int][]int)
+	for idx, point := range points {
+		distance := point[0]*point[0] + point[1]*point[1]
+		if len(hash[distance]) == 0 {
+			hash[distance] = []int{idx}
+			fmt.Println(hash[distance])
+			distances = append(distances, distance)
+		} else {
+			hash[distance] = append(hash[distance], idx)
+		}
+	}
+	fmt.Println(distances, hash, k)
+	sort.Ints(distances)
+	ans := [][]int{}
+	for i := 0; i < k; {
+		for _, idx := range hash[distances[i]] {
+			if len(ans) >= k {
+				break
+			}
+			ans = append(ans, points[idx])
+			i++
+		}
+
+	}
+	return ans[:k]
+}
+
+func kClosestv1(points [][]int, k int) [][]int {
+	if len(points) == 1 && k == 1 {
+		return points
+	}
+	sort.Slice(points, func(i, j int) bool {
+		p, q := points[i], points[j]
+		return p[0]*p[0]+p[1]*p[1] < q[0]*q[0]+q[1]*q[1]
+	})
+	return points[:k]
+}
