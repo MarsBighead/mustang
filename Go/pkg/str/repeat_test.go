42c403c79556de5b1bc45d8a08ba3d7c5d9db024
MarsBighead <duanmhy@126.com>
2024-12-30 17:41:33 +0800

    String  matching in an array

--- Go/pkg/str/repeat_test.go
+++ Go/pkg/str/repeat_test.go
@@ -18,3 +18,39 @@ func TestRepeatedSubstringPattern(t *testing.T) {
 	}
 
 }
+
+func TestMaxRepeating(t *testing.T) {
+	ss := []string{
+		"ababc",
+		"ababc",
+		"ababc",
+		"aaabaaaabaaabaaaabaaaabaaaabaaaaba",
+		"aaa",
+		"baba",
+	}
+	words := []string{
+		"ab",
+		"ba",
+		"ac",
+		"aaaba",
+		"a",
+		"b",
+	}
+	for i, s := range ss {
+		fmt.Printf("org=%s\n", s)
+		num := maxRepeatingv1(s, words[i])
+		fmt.Printf("num=%d\n\n", num)
+	}
+
+}
+
+func TestStringMatching(t *testing.T) {
+	wordss := [][]string{
+		{"mass", "as", "hero", "superhero"},
+		{"leetcode", "et", "code"},
+	}
+	for _, words := range wordss {
+		ans := stringMatchingv1(words)
+		fmt.Println(ans)
+	}
+}
