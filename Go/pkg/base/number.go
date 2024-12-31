8ccb13742c7257c12aa63a98f7fb3fcfe613a137
MarsBighead <duanmhy@126.com>
2024-12-31 14:10:23 +0800

    Number base 7

---
+++ Go/pkg/base/number.go
@@ -0,0 +1,31 @@
+package base
+
+import (
+	"fmt"
+	"strconv"
+)
+
+// 504. 七进制数
+// https://leetcode.cn/problems/base-7/description/
+func convertToBase7(num int) string {
+	if num < 7 && num > -7 {
+		return fmt.Sprintf("%d", num)
+	}
+	ans := ""
+	isNagetive := false
+	if num < 0 {
+		num = -num
+		isNagetive = true
+	}
+	for num > 0 {
+		ans = strconv.Itoa(num%7) + ans
+		num = num / 7
+	}
+	//fmt.Println(num)
+
+	if isNagetive {
+		ans = "-" + ans
+	}
+
+	return ans
+}
