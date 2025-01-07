e1a3612e0209893186ae89b9e7f61ffd4b8d061e
MarsBighead <duanmhy@126.com>
2025-01-07 15:35:15 +0800

    Add loop method to build tree

--- Go/pkg/gtree/build_test.go
+++ Go/pkg/gtree/build_test.go
@@ -8,7 +8,18 @@ import (
 func TestBuildTree(t *testing.T) {
 	preorder := []int{3, 9, 20, 15, 7}
 	inorder := []int{9, 3, 15, 20, 7}
-	root := buildTree(preorder, inorder)
+	root := buildTreev1(preorder, inorder)
+
+	fmt.Printf("%#v\n", root)
+	fmt.Printf("%#v\n", preorder)
+	order := preorderTraversal(root)
+	fmt.Printf("%#v\n", order)
+}
+
+func TestBuildTreev1(t *testing.T) {
+	preorder := []int{1, 2, 4, 5, 6, 7, 3, 8, 9}
+	inorder := []int{4, 2, 6, 5, 7, 1, 9, 8, 3}
+	root := buildTreev1(preorder, inorder)
 
 	fmt.Printf("%#v\n", root)
 	fmt.Printf("%#v\n", preorder)
