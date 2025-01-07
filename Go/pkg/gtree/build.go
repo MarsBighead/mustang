be8399db5cae818d14178e4b1f2f29dd725707b3
MarsBighead <duanmhy@126.com>
2025-01-07 15:35:15 +0800

    Add loop method to build tree

--- Go/pkg/gtree/build.go
+++ Go/pkg/gtree/build.go
@@ -17,3 +17,29 @@ func buildTree(preorder []int, inorder []int) *TreeNode {
 	root.Right = buildTree(preorder[i+1:], inorder[i+1:])
 	return root
 }
+
+func buildTreev1(preorder []int, inorder []int) *TreeNode {
+	if len(preorder) == 0 {
+		return nil
+	}
+	root := &TreeNode{preorder[0], nil, nil}
+	idx := 0
+	stack := []*TreeNode{root}
+	for i := 1; i < len(preorder); i++ {
+		preorderVal := preorder[i]
+		node := stack[len(stack)-1]
+		if node.Val != inorder[idx] {
+			node.Left = &TreeNode{preorderVal, nil, nil}
+			stack = append(stack, node.Left)
+		} else {
+			for len(stack) != 0 && stack[len(stack)-1].Val == inorder[idx] {
+				node = stack[len(stack)-1]
+				stack = stack[:len(stack)-1]
+				idx++
+			}
+			node.Right = &TreeNode{preorderVal, nil, nil}
+			stack = append(stack, node.Right)
+		}
+	}
+	return root
+}
