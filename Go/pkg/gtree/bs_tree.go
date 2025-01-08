1e4af5630ae1ef4bb838b1498782027fddf17b2a
MarsBighead <duanmhy@126.com>
2025-01-08 17:18:48 +0800

    Add case insert into a binary search tree

--- Go/pkg/gtree/bs_tree.go
+++ Go/pkg/gtree/bs_tree.go
@@ -76,3 +76,29 @@ func searchBSTv1(root *TreeNode, val int) *TreeNode {
 	}
 	return ans
 }
+
+// 701. 二叉搜索树中的插入操作
+// https://leetcode.cn/problems/insert-into-a-binary-search-tree/description/
+// 该解答未实现插入值，替换原来节点，原有节点移动重排的情况
+func insertIntoBST(root *TreeNode, val int) *TreeNode {
+	if root == nil {
+		return &TreeNode{Val: val}
+	}
+	node := root
+	for node != nil {
+		if node.Val > val {
+			if node.Left == nil {
+				node.Left = &TreeNode{Val: val}
+				break
+			}
+			node = node.Left
+		} else if node.Val < val {
+			if node.Right == nil {
+				node.Right = &TreeNode{Val: val}
+				break
+			}
+			node = node.Right
+		}
+	}
+	return root
+}
