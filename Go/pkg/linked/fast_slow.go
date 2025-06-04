84a147fd27a2b47135c1932bcd13070c725c6c47
MarsBighead <duanmhy@126.com>
2025-06-04 15:07:47 +0800

    Add majority element case

--- Go/pkg/linked/fast_slow.go
+++ Go/pkg/linked/fast_slow.go
@@ -1,5 +1,7 @@
 package linked
 
+import "sort"
+
 // https://leetcode.cn/problems/remove-duplicates-from-sorted-array-ii/?envType=study-plan-v2&envId=top-interview-150
 // 删除排序数组中的重复项 II
 func removeDuplicates(nums []int) int {
@@ -15,4 +17,31 @@ func removeDuplicates(nums []int) int {
 		}
 	}
 	return left
+}
+
+// https://leetcode.cn/problems/majority-element/description/
+// 169. 多数元素
+func majorityElement(nums []int) int {
+	sort.Ints(nums)
+	return nums[len(nums)/2]
+}
+
+// Boyer-Moore 算法
+// Reference: https://leetcode.cn/problems/majority-element/solutions/1/169-duo-shu-yuan-su-mo-er-tou-piao-qing-ledrh/
+func majorityElementv1(nums []int) int {
+	candidate, count := -1, 0
+	for _, num := range nums {
+		if num == candidate {
+			count++
+		} else {
+			if count == 0 {
+				candidate = num
+				count = 1
+			} else {
+				count--
+			}
+		}
+	}
+	return candidate
+
 }
