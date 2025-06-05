4de5ecca9965989afbf0ebc24b871832b4fc4d76
MarsBighead <duanmhy@126.com>
2025-06-05 11:02:16 +0800

    Check is subsequence

--- Go/pkg/str/word.go
+++ Go/pkg/str/word.go
@@ -93,3 +93,20 @@ func convert(s string, numRows int) string {
 	return string(result)
 
 }
+
+// https://leetcode.cn/problems/is-subsequence/description
+// 392. 判断子序列
+func isSubsequence(s string, t string) bool {
+	m, n := len(s), len(t)
+	if m > n {
+		return false
+	}
+	i, j := 0, 0
+	for i < m && j < n {
+		if s[i] == t[j] {
+			i++
+		}
+		j++
+	}
+	return m == i
+}
