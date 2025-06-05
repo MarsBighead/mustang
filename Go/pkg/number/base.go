dd1d8657595510bd44f40eb9be5725f47943c380
MarsBighead <duanmhy@126.com>
2025-06-05 23:13:03 +0800

    Case of longest consecutive sequence

--- Go/pkg/number/base.go
+++ Go/pkg/number/base.go
@@ -15,3 +15,36 @@ func isPalindrome(x int) bool {
 	return x == reversed || x == reversed/10
 
 }
+
+// https://leetcode.cn/problems/longest-consecutive-sequence/description/?envType=study-plan-v2&envId=top-interview-150
+// 128. 最长连续序列
+func longestConsecutive(nums []int) int {
+	n := len(nums)
+	if n == 0 {
+		return 0
+	}
+	hash := make(map[int]bool, n)
+
+	for _, num := range nums {
+		hash[num] = true
+	}
+	if len(hash) == 1 {
+		return 1
+	}
+	ans := 0
+	for num, _ := range hash {
+		if !hash[num-1] {
+			current := num
+			cnt := 1
+			for hash[current+1] {
+				current++
+				cnt++
+			}
+			if ans < cnt {
+				ans = cnt
+			}
+		}
+
+	}
+	return ans
+}
