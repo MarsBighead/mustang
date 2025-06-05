d48a07d8e767b3206c622f9e4e58f6ce006dfaf4
MarsBighead <duanmhy@126.com>
2025-06-05 10:35:41 +0800

    find the index of the first occurrence in a string

--- Go/pkg/str/word.go
+++ Go/pkg/str/word.go
@@ -1,6 +1,9 @@
 package str
 
-import "strings"
+import (
+	"regexp"
+	"strings"
+)
 
 // https://leetcode.cn/problems/length-of-last-word
 // 58. 最后一个单词的长度
@@ -28,4 +31,38 @@ func lengthOfLastWordv1(s string) int {
 		ans++
 	}
 	return ans
+}
+
+// https://leetcode.cn/problems/find-the-index-of-the-first-occurrence-in-a-string
+func strStr(haystack string, needle string) int {
+	if len(needle) > len(haystack) {
+		return -1
+	}
+	re := regexp.MustCompile(needle)
+	indices := re.FindStringIndex(haystack)
+	if indices != nil {
+		return indices[0]
+	}
+	return -1
+}
+
+func strStrv1(haystack string, needle string) int {
+	n, m := len(haystack), len(needle)
+	if m > n {
+		return -1
+	}
+
+	for i := 0; i < n; i++ {
+		if haystack[i] == needle[0] {
+			if i+m <= n {
+				cap := haystack[i : i+m]
+				if cap == needle {
+					return i
+				}
+			} else {
+				return -1
+			}
+		}
+	}
+	return -1
 }
