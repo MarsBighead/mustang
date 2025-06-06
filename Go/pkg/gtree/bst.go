4d4dc6ec67fe606688fba8471c74ac332df965ac
MarsBighead <duanmhy@126.com>
2025-06-06 17:10:01 +0800

    Case#108 Convertsorted array to binary search tree

--- Go/pkg/gtree/bst.go
+++ Go/pkg/gtree/bst.go
@@ -1,1 +1,16 @@
 package gtree
+
+// 108. 将有序数组转换为二叉搜索树
+func sortedArrayToBST(nums []int) *TreeNode {
+	n := len(nums)
+	if n == 0 {
+		return nil
+	}
+	mid := n / 2
+	root := &TreeNode{
+		Val:   nums[mid],
+		Left:  sortedArrayToBST(nums[:mid]),
+		Right: sortedArrayToBST(nums[mid+1:]),
+	}
+	return root
+}
