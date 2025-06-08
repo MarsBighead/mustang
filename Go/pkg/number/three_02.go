e734535b42b9dc68d938b19959199db4721ffddc
MarsBighead <duanmhy@126.com>
2025-06-08 10:38:12 +0800

    Case#279 Perfect squares

--- Go/pkg/number/three_02.go
+++ Go/pkg/number/three_02.go
@@ -1,6 +1,9 @@
 package number
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 // https://leetcode.cn/problems/bitwise-and-of-numbers-range/?envType=study-plan-v2&envId=top-interview-150
 // 201. 数字范围按位与
@@ -12,4 +15,33 @@ func rangeBitwiseAnd(left int, right int) int {
 
 	}
 	return right
+}
+
+// 279. 完全平方数
+// https://leetcode.cn/problems/perfect-squares/description/?envType=study-plan-v2&envId=top-100-liked
+// 四平方和定理
+func numSquares(n int) int {
+	isSquare := func(x int) bool {
+		y := int(math.Sqrt(float64(x)))
+		return x == y*y
+	}
+	//4^k*(8*m+7)
+	is4Squares := func(x int) bool {
+		for x%4 == 0 {
+			x /= 4
+		}
+		return x%8 == 7
+	}
+	if isSquare(n) {
+		return 1
+	}
+	if is4Squares(n) {
+		return 4
+	}
+	for i := 1; i*i < n; i++ {
+		if isSquare(n - i*i) {
+			return 2
+		}
+	}
+	return 3
 }
