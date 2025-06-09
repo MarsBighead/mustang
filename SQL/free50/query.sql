4c9a19932f7aa8ec2b25d60550f42fa50d7d666c
MarsBighead <duanmhy@126.com>
2025-06-09 10:06:48 +0800

    Case#1934 Confirmation rate

--- SQL/free50/query.sql
+++ SQL/free50/query.sql
@@ -24,3 +24,33 @@ order by author_id
 -- 1683. 无效的推文
 select tweet_id from Tweets
 where length(content)>15
+
+
+-- Write your PostgreSQL query statement below
+-- 1934. 确认率
+-- https://leetcode.cn/problems/confirmation-rate/description/?envType=study-plan-v2&envId=sql-free-50
+select s.user_id, (case 
+  when a.count is not null then round(cast(t.count as numeric)/a.count,2)
+  else 0 end) confirmation_rate
+from Signups s left join
+    (select user_id, count(user_id) count
+        from Confirmations 
+    where action='confirmed'
+    group by user_id) t
+on s.user_id=t.user_id left join 
+    (select user_id, 
+    count(user_id) count
+    from Confirmations
+    group by user_id) a
+on t.user_id=a.user_id
+
+/*对 case when...else end的运用，以及NUM类型转数字的方法*/
+select s.user_id, (case 
+    when count(t.user_id) = 0 then 0
+    else round(SUM(
+        CASE WHEN t.action = 'confirmed' THEN 1 ELSE 0 END)::numeric 
+        /count(t.user_id),2)
+    end) confirmation_rate
+from Signups s left join Confirmations t
+on s.user_id=t.user_id
+group by s.user_id
