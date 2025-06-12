8b3bc0b369c68dec60a19d181b4b08dd40eef991
MarsBighead <duanmhy@126.com>
2025-06-12 16:38:36 +0800

    Go channel use case and unexception type

--- Go/time-out/main.go
+++ Go/time-out/main.go
@@ -10,6 +10,16 @@ func main() {
 	ctx, cancel := context.WithTimeout(context.TODO(), time.Second*3)
 	defer cancel()
 	go task(ctx)
+	var ch chan int
+	// read from uninitialized chan
+	// x := <-ch /// fatal error: all goroutines are asleep - deadlock!
+	// fmt.Println("x from chan", x)
+	// write to uninitialized chan
+	//ch <- 1             /// fatal error:: all goroutines are asleep - deadlock!
+	ch = make(chan int) // init channel
+	close(ch)
+	// close(ch) // panic: close of closed channel
+	// ch <- 1 // panic: send on closed channel
 	time.Sleep(time.Second * 6)
 }
 
