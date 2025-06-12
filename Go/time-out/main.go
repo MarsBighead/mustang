e825d038bb01375d2f9e85f52d70aed04dfb1ea7
MarsBighead <duanmhy@126.com>
2025-06-12 16:43:22 +0800

    Recommand practice to avoid Go channel excessive blockage

--- Go/time-out/main.go
+++ Go/time-out/main.go
@@ -20,6 +20,8 @@ func main() {
 	close(ch)
 	// close(ch) // panic: close of closed channel
 	// ch <- 1 // panic: send on closed channel
+	v := <-ch // 缓冲取完，就是0（int零值）
+	fmt.Println("value after channel closed", v)
 	time.Sleep(time.Second * 6)
 }
 
@@ -37,4 +39,40 @@ func task(ctx context.Context) {
 	case <-ctx.Done():
 		fmt.Println("timeout")
 	}
+}
+
+// practice
+func practice() {
+	// 不佳：每次循环都阻塞
+	ch := make(chan int)
+	for {
+		data := <-ch // 频繁 gopark/goready
+		process(data)
+	}
+	buffer := []int{}
+	batchSize := 10
+	// 优化：批量处理
+	for {
+		select {
+		case data := <-ch:
+			buffer = append(buffer, data)
+			if len(buffer) >= batchSize {
+				processBatch(buffer)
+				buffer = buffer[:0]
+			}
+		case <-time.After(time.Millisecond):
+			if len(buffer) > 0 {
+				processBatch(buffer)
+				buffer = buffer[:0]
+			}
+		}
+	}
+}
+
+func process(data int) {
+	fmt.Println("data", data)
+}
+
+func processBatch(buffer []int) {
+	fmt.Println("data", buffer)
 }
