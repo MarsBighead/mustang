4787e7a06e620ffc51b3010f0f81a45353152030
MarsBighead <duanmhy@126.com>
2025-06-16 10:28:32 +0800

    Case#32 valid sudoku

--- Go/pkg/garray/double.go
+++ Go/pkg/garray/double.go
@@ -73,6 +73,31 @@ func searchInsert(nums []int, target int) int {
 	return n
 }
 
+// https://leetcode.cn/problems/valid-sudoku/description/?envType=study-plan-v2&envId=top-interview-150
+// 36. 有效的数独
+func isValidSudoku(board [][]byte) bool {
+	var (
+		rows, columns [9][9]int
+		subboxes      [3][3][9]int
+	)
+	for i, row := range board {
+		for j, c := range row {
+			if c == '.' {
+				continue
+			}
+			index := c - '1'
+			rows[i][index]++
+			columns[j][index]++
+			subboxes[i/3][j/3][index]++
+			if rows[i][index] > 1 || columns[j][index] > 1 || subboxes[i/3][j/3][index] > 1 {
+				return false
+			}
+		}
+	}
+	return true
+
+}
+
 // https://leetcode.cn/problems/combination-sum/
 // 39. Combination Sum(组合总和)
 func CombinationSum(candidates []int, target int) (ans [][]int) {
